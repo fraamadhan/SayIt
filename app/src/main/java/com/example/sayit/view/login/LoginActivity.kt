fa4ccf2cb84a5099dce95e8675220ad708d5ec5b
fraamadhan <fakhrifajarrr@gmail.com>
2023-11-22 14:59:01 +0700

    feat: splash screen

--- app/src/main/java/com/example/sayit/view/login/LoginActivity.kt
+++ app/src/main/java/com/example/sayit/view/login/LoginActivity.kt
@@ -1,11 +1,12 @@
-package com.example.sayit
+package com.example.sayit.view.login
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.sayit.R
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
     }
 }
